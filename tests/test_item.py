d22dfdd873383a072b1241010a8612dcc9a8d550
Dmitry <kde98@mail.ru>
2023-12-25 18:31:08 +0300

    write tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,20 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+def test___init__():
+    item = Item("Laptop", 5000, 3)
+    assert item.name == "Laptop"
+    assert item.price == 5000
+    assert item.quantity == 3
+
+def test_calculate_total_price():
+    item = Item("Laptop", 5000, 3)
+    assert item.calculate_total_price() == 15000
+
+def test_apply_discount():
+    item = Item("Laptop", 5000, 3)
+    item.pay_rate = 0.8
+    assert item.apply_discount() is None
+    item.apply_discount()
+    assert item.price == 3200.0
