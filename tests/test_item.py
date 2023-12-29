7ed489586557edf9b9eff2d0430aee7bcf53ce80
Dmitry <kde98@mail.ru>
2023-12-29 16:44:03 +0300

    complete hw2

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,16 +1,20 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-import pytest
 from src.item import Item
+import pytest
 
+
+@pytest.fixture
 def test___init__():
     item = Item("Laptop", 5000, 3)
     assert item.name == "Laptop"
     assert item.price == 5000
     assert item.quantity == 3
 
+
 def test_calculate_total_price():
     item = Item("Laptop", 5000, 3)
     assert item.calculate_total_price() == 15000
+
 
 def test_apply_discount():
     item = Item("Laptop", 5000, 3)
@@ -18,3 +22,9 @@ def test_apply_discount():
     assert item.apply_discount() is None
     item.apply_discount()
     assert item.price == 3200.0
+
+
+def test_string_to_number():
+    assert Item.string_to_number('10') == 10
+    assert Item.string_to_number('5.5') == 5
+    assert Item.string_to_number('abc') == 'abc'
