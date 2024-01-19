c099a6a308f696bedcc5e9dee0777ac2b468079d
Dmitry <kde98@mail.ru>
2024-01-19 19:07:37 +0300

    add class phone

---
+++ src/phone.py
@@ -0,0 +1,22 @@
+from src.item import Item
+class Phone(Item):
+    def __init__(self, name: str, price: float, quantity: int, sim: int):
+        super().__init__(name, price, quantity)
+        self._sim = sim
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self._sim})"
+
+    @property
+    def number_of_sim(self):
+        return self._sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, quantity):
+        if isinstance(quantity, int) and quantity > 0:
+            self._sim = quantity
+        else:
+            raise ValueError('Число должно быть больше нуля')
